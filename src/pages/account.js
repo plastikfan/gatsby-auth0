00f470b721ec5035b71bbfae20e34d89028504ca
plastikfan <plastikfan@outlook.com>
2021-12-13 17:10:26 +0000

    create client side only route

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,11 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// NB: gatsby turns all files in the pages folder into a route that exports a react component.
-//
+const Home = () => <p>Home</p>
+const Settings = () => <p>Settings</p>
+const Billing = () => <p>Billing</p>
+
 const Account = () => (
-  <div>
-    <p>This is going to be a protected route.</p>
-  </div>
+  <BrowserRouter>
+    <nav>
+      <Link to="/account">Home</Link>{" "}
+      <Link to="/account/settings">Settings</Link>{" "}
+      <Link to="/account/billing">Billing</Link>{" "}
+    </nav>
+    <Routes>
+      <Route path="/account" element={<Home />} />
+      <Route path="/account/settings" element={<Settings />} />
+      <Route path="/account/billing" element={<Billing />} />
+    </Routes>
+  </BrowserRouter>
 )
 
 export default Account
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Home = () => {
+const Index = () => {
   return <div>
     <p>Hello Gatsby</p>
     <Link to="/account">Go to your account</Link>
   </div>
 }
 
-export default Home;
+export default Index;
