0d4828abd89e53c8c085ab42d5e58a22ec52cdb4
plastikfan <plastikfan@outlook.com>
2021-12-13 20:23:39 +0000

    implement login

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,24 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { login, isAuthenticated, getProfile } from "../utils/auth"
 
-const Home = () => <p>Home</p>
+const Home = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
+
 const Settings = () => <p>Settings</p>
 const Billing = () => <p>Billing</p>
 
-const Account = () => (
-  <BrowserRouter>
-    <nav>
-      <Link to="/account">Home</Link>{" "}
-      <Link to="/account/settings">Settings</Link>{" "}
-      <Link to="/account/billing">Billing</Link>{" "}
-    </nav>
-    <Routes>
-      <Route path="/account" element={<Home />} />
-      <Route path="/account/settings" element={<Settings />} />
-      <Route path="/account/billing" element={<Billing />} />
-    </Routes>
-  </BrowserRouter>
-)
+const Account = () => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
+  const user = getProfile()
+
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/account">Home</Link>{" "}
+        <Link to="/account/settings">Settings</Link>{" "}
+        <Link to="/account/billing">Billing</Link>{" "}
+      </nav>
+      <Routes>
+        <Route path="/account" element={<Home user={user} />} />
+        <Route path="/account/settings" element={<Settings />} />
+        <Route path="/account/billing" element={<Billing />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default Account
