c660726b1d4e8a2c01dda5fba57660f801de28a4
plastikfan <plastikfan@outlook.com>
2021-12-13 16:44:49 +0000

    create protected account route

---
+++ src/pages/account.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+// NB: gatsby turns all files in the pages folder into a route that exports a react component.
+//
+const Account = () => (
+  <div>
+    <p>This is going to be a protected route.</p>
+  </div>
+)
+
+export default Account
